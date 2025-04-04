d6a6a92d49b714dd26094f50d322c4038981b3c8
PPWangyc <ppwangyc@gmail.com>
2025-04-04 14:04:46 -0400

    fix vit-b-sam load model issue during perdiction

--- lightning_pose/utils/scripts.py
+++ lightning_pose/utils/scripts.py
@@ -340,7 +340,7 @@ def get_loss_factories(
 def get_model(
     cfg: DictConfig,
     data_module: BaseDataModule | UnlabeledDataModule,
-    loss_factories: dict[str, LossFactory],
+    loss_factories: dict[str, LossFactory] | dict[str, None],
 ) -> pl.LightningModule:
     """Create model: regression or heatmap based, supervised or semi-supervised."""
 
@@ -378,14 +378,26 @@ def get_model(
                 model_path=model_path,  # pass model_path to backbone
             )
         elif cfg.model.model_type == "heatmap":
+            if data_module:
+                 output_shape = data_module.dataset.output_shape
+                 num_targets = data_module.dataset.num_targets
+            else:
+                 # @Yanchen Wang: this need to be adjusted for each dataset
+                 # date: 2025-04-04
+                 output_shape = (64, 64)
+                 num_targets = cfg.data.num_keypoints * 2 
+            # print(f"num_keypoints: {cfg.data.num_keypoints} \
+            #       learning_rate: {optimizer_params['learning_rate']} \
+            #       num_targets: {num_targets} \
+            #       output_shape: {output_shape}")
             model = HeatmapTracker(
                 num_keypoints=cfg.data.num_keypoints,
-                num_targets=data_module.dataset.num_targets,
+                num_targets=num_targets,
                 loss_factory=loss_factories["supervised"],
                 backbone=cfg.model.backbone,
                 pretrained=backbone_pretrained,
                 downsample_factor=cfg.data.get("downsample_factor", 2),
-                output_shape=data_module.dataset.output_shape,
+                output_shape=output_shape,
                 torch_seed=cfg.training.rng_seed_model_pt,
                 optimizer=optimizer,
                 optimizer_params=optimizer_params,
